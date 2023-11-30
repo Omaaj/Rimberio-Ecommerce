8d5edc421152f5a3e8dd12d503b3244e94d15f3d
Omaaj <juwiz999@gmail.com>
2023-11-30 04:56:30 -0800

    updated

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -56,7 +56,7 @@ export default function Contact() {
                     />
                     <textarea 
                       name='message' 
-                      placeholder='Your Message' 
+                      placeholder='Enter Your Message' 
                       required 
                     />
                     <button type='submit'>Send</button>
