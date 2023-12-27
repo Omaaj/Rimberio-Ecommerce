a409f53fc5ad7ca80d23506bd3cc0538e61d20ca
Omaaj <juwiz999@gmail.com>
2023-12-27 08:34:41 -0500

    updated

--- src/components1/adminRu/AdminRouter.jsx
+++ src/components1/adminRu/AdminRouter.jsx
@@ -1,15 +1,69 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectEmail } from '../../redux/slice/authSlice'
+// import React from 'react'
+// import { useSelector } from 'react-redux'
+// import { selectEmail } from '../../redux/slice/authSlice'
+// import { Link } from 'react-router-dom';
+
+// const AdminRouter = ({children})  => {
+//   const userEmail = useSelector(selectEmail);
+
+//   const bothEmail = {
+//     first : "walkerjayden2423@gmail.com",
+//     second : "wilberanthony2423@gmail.com"
+//   }
+
+//   if(userEmail === bothEmail) {
+//     return children
+//   }
+//   // if(userEmail === process.env.REACT_APP_ADMIN_USER) {
+//   //   return children
+//   // }
+//   return(
+//     <section>
+//       <div className="container">
+//         <h2>Permission Denied</h2>
+//         <p>This page can only be viewed by admin user</p>
+//       </div>
+//       <br />
+//       <Link to="/">
+//         <button>&larr; Back To home</button>
+//       </Link>
+//     </section>
+//   );
+// }
+
+// export function AdminLink({children}) {
+//   const userEmail = useSelector(selectEmail);
+
+//   const bothEmail = {
+//     first : "walkerjayden2423@gmail.com",
+//     second : "wilberanthony2423@gmail.com"
+//   }
+
+//   if(userEmail === bothEmail) {
+//     return children;
+//   }
+//   // if(userEmail === process.env.REACT_APP_ADMIN_USER) {
+//   //   return children;
+//   // }
+//   return null;
+// }
+
+// export default AdminRouter
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectEmail } from '../../redux/slice/authSlice';
 import { Link } from 'react-router-dom';
 
-const AdminRouter = ({children})  => {
+const AdminRouter = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
-  if(userEmail === process.env.REACT_APP_ADMIN_USER) {
-    return children
+  const allowedEmails = [process.env.REACT_APP_ADMIN_USER1, process.env.REACT_APP_ADMIN_USER];
+
+  if (allowedEmails.includes(userEmail)) {
+    return children;
   }
-  return(
+
+  return (
     <section>
       <div className="container">
         <h2>Permission Denied</h2>
@@ -21,15 +75,18 @@ const AdminRouter = ({children})  => {
       </Link>
     </section>
   );
-}
+};
 
-export function AdminLink({children}) {
+export function AdminLink({ children }) {
   const userEmail = useSelector(selectEmail);
 
-  if(userEmail === process.env.REACT_APP_ADMIN_USER) {
+  const allowedEmails = [process.env.REACT_APP_ADMIN_USER1, process.env.REACT_APP_ADMIN_USER];
+
+  if (allowedEmails.includes(userEmail)) {
     return children;
   }
+
   return null;
 }
 
-export default AdminRouter
+export default AdminRouter;
